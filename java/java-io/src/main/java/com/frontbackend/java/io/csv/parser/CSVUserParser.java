2707321c4f63ac4adb422b5d478809fc417f1cd1
D13 <rolandoroz@gmail.com>
2021-03-19 21:12:05 +0100

    Delete java directory

--- java/java-io/src/main/java/com/frontbackend/java/io/csv/parser/CSVUserParser.java
+++
@@ -1,6 +0,0 @@
-package com.frontbackend.java.io.csv.parser;
-
-import com.frontbackend.java.io.csv.model.User;
-
-public class CSVUserParser extends CSVAbstractParser<User> {
-}
--- java/java-io/src/main/java/com/frontbackend/java/io/trywithresources/MyCustomAutoCloseableResource.java
+++
@@ -1,9 +0,0 @@
-package com.frontbackend.java.io.trywithresources;
-
-public class MyCustomAutoCloseableResource implements AutoCloseable {
-
-    @Override
-    public void close() throws Exception {
-        // close resource
-    }
-}
